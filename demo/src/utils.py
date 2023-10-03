883f80d5925b199601070ef3020aad603c5e3393
André Pedersen <andrped94@gmail.com>
2023-10-03 18:52:26 +0200

    Fixed local demo launch

--- demo/src/utils.py
+++ demo/src/utils.py
@@ -1,5 +1,7 @@
 import nibabel as nib
 import numpy as np
+from nibabel.processing import resample_to_output
+from skimage.measure import marching_cubes
 
 
 def load_ct_to_numpy(data_path):
@@ -36,3 +38,28 @@ def load_pred_volume_to_numpy(data_path):
 
     print(data.shape)
     return [data[..., i] for i in range(data.shape[-1])]
+
+
+def nifti_to_glb(path, output="prediction.obj"):
+    # load NIFTI into numpy array
+    image = nib.load(path)
+    resampled = resample_to_output(image, [1, 1, 1], order=1)
+    data = resampled.get_fdata().astype("uint8")
+
+    # extract surface
+    verts, faces, normals, values = marching_cubes(data, 0)
+    faces += 1
+
+    with open(output, "w") as thefile:
+        for item in verts:
+            thefile.write("v {0} {1} {2}\n".format(item[0], item[1], item[2]))
+
+        for item in normals:
+            thefile.write("vn {0} {1} {2}\n".format(item[0], item[1], item[2]))
+
+        for item in faces:
+            thefile.write(
+                "f {0}//{0} {1}//{1} {2}//{2}\n".format(
+                    item[0], item[1], item[2]
+                )
+            )
