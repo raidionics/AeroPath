27f2632e5a021998acc33684262e3b542feef156
André Pedersen <andrped94@gmail.com>
2023-10-31 00:53:03 +0100

    Added flush_logs method

--- demo/src/logger.py
+++ demo/src/logger.py
@@ -3,14 +3,14 @@ import sys
 
 
 def get_logger():
-    logging.getLogger(__name__)
+    return logging.getLogger(__name__)
 
 
 def setup_logger():
     # clear log
     file_to_delete = open("log.txt", "w")
     file_to_delete.close()
-
+    
     file_handler = logging.FileHandler(filename="log.txt")
     stdout_handler = logging.StreamHandler(stream=sys.stdout)
     handlers = [file_handler, stdout_handler]
@@ -28,3 +28,10 @@ def read_logs():
     sys.stdout.flush()
     with open("log.txt", "r") as f:
         return f.read()
+
+
+def flush_logs():
+    sys.stdout.flush()
+    # clear log
+    file_to_delete = open("log.txt", "w")
+    file_to_delete.close()
